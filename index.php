76b99566b79574ad965c9c16e9fa3aa03b25a5df
lise <lise.p.poirier@gmail.com>
2016-09-04 11:47:20 +0200

    Mise en place NAV

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" author="Lise Poirier">
+    <title>Welcome</title>
+	
+	<!-- Scripts -->
+	<script src="js/jquery.js"></script>
+  	<script src="js/bootstrap.js"></script>
+  	
+  	<!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css"/>
+	<link rel="stylesheet" type="text/css" href="css/style.css"/>
+  	
+  	<!-- Fonts -->
+  	<link type="text/css" rel="stylesheet" href="css/font-awesome.css"/>
+  	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+
+</head>
+<body>
+   <div class="container-fluid">
+       <?php include 'nav.php'; ?>
+   </div>
+    
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,20 @@
+<nav class="navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle bouton_nav" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar icon_nav"></span>
+        <span class="icon-bar icon_nav"></span>
+        <span class="icon-bar icon_nav"></span>                        
+      </button>
+      <a id="startbootstrap_nav" class="navbar-brand" href="#">hello word</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li class="active"><a class="liens_nav" href="#">ACCUEIL</a></li>
+        <li class="active"><a class="liens_nav" href="#">WEB</a></li>
+        <li class="active"><a class="liens_nav" href="#">BACK</a></li>
+        <li class="active"><a class="liens_nav" href="#">CONTACT</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>

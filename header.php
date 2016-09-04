1c6e35fde90f98e17574783e08e4cd5c49f4c1b0
lise <lise.p.poirier@gmail.com>
2016-09-04 15:19:18 +0200

    indexOk

--- header.php
+++ header.php
@@ -4,15 +4,15 @@
     <div class="separator-header"></div>
     <div class="separator-header2"></div>
     <div class="row">
-        <div class="col-md-offset-2 col-md-2">
+        <div class="col-xs-offset-1 col-xs-10 col-md-offset-2 col-md-2 col-sm-offset-2 col-sm-2">
             <i id='animation-header'class="fa fa-heart fa-5x" aria-hidden="true"></i>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam deleniti iure aliquam quibusdam doloremque id repudiandae quae aliquid mollitia sed eaque, libero esse quo dignissimos, accusamus atque pariatur distinctio velit!</p>
         </div>
-        <div class="col-md-offset-1 col-md-2">
+        <div class="col-xs-offset-1 col-xs-10  col-md-offset-1 col-md-2 col-sm-offset-1 col-sm-2">
             <i id='animation-header'class="fa fa-paper-plane fa-5x" aria-hidden="true"></i>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem autem molestias aperiam, quae consectetur ipsam ducimus dolore minus voluptatum sunt provident libero veniam corporis laborum ut voluptatibus quia natus.</p>
         </div>
-        <div class="col-md-offset-1 col-md-2">
+        <div class="col-xs-offset-1 col-xs-10  col-md-offset-1 col-md-2 col-sm-offset-1 col-sm-2">
             <i id='animation-header'class="fa fa-comments fa-5x" aria-hidden="true"></i>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus explicabo, aspernatur nobis exercitationem quis dolorem aliquid nihil assumenda deserunt eaque quo fugit nam, excepturi quasi modi debitis ex provident quae.</p>
         </div>
--- portfolio.php
+++ portfolio.php
@@ -1,65 +1,65 @@
 <div class="portfolio row center-block">
           
           
-           <div class="projet col-md-4" id="projet1">
+           <div class="projet col-md-4 col-sm-6" id="projet1">
                        <div class="projet-present">
-                       <h3>Nom Projet</h3>
+                       <a href="#"><h3>Nom Projet</h3></a>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut vitae suscipit nobis hic, facere ipsam ad ratione explicabo inventore blanditiis eius provident unde, repudiandae necessitatibus nam voluptatem, iusto delectus! Consectetur!.</p>
                         </div>
             </div>
             
-            <div class="projet col-md-4" id="projet2">
+            <div class="projet col-md-4 col-sm-6" id="projet2">
                        <div class="projet-present">
-                       <h3>Nom Projet</h3>
+                       <a href="#"><h3>Nom Projet</h3></a>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut vitae suscipit nobis hic, facere ipsam ad ratione explicabo inventore blanditiis eius provident unde, repudiandae necessitatibus nam voluptatem, iusto delectus! Consectetur!.</p>
                         </div>
             </div>
             
-            <div class="projet col-md-4" id="projet3">
+            <div class="projet col-md-4 col-sm-6" id="projet3">
                        <div class="projet-present">
-                       <h3>Nom Projet</h3>
+                       <a href="#"><h3>Nom Projet</h3></a>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut vitae suscipit nobis hic, facere ipsam ad ratione explicabo inventore blanditiis eius provident unde, repudiandae necessitatibus nam voluptatem, iusto delectus! Consectetur!.</p>
                         </div>
             </div>
             
-            <div class="projet col-md-4" id="projet4">
+            <div class="projet col-md-4 col-sm-6" id="projet4">
                        <div class="projet-present">
-                       <h3>Nom Projet</h3>
+                       <a href="#"><h3>Nom Projet</h3></a>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut vitae suscipit nobis hic, facere ipsam ad ratione explicabo inventore blanditiis eius provident unde, repudiandae necessitatibus nam voluptatem, iusto delectus! Consectetur!.</p>
                         </div>
             </div>
             
-            <div class="projet col-md-4" id="projet5">
+            <div class="projet col-md-4 col-sm-6" id="projet5">
                        <div class="projet-present">
-                       <h3>Nom Projet</h3>
+                       <a href="#"><h3>Nom Projet</h3></a>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut vitae suscipit nobis hic, facere ipsam ad ratione explicabo inventore blanditiis eius provident unde, repudiandae necessitatibus nam voluptatem, iusto delectus! Consectetur!.</p>
                         </div>
             </div>
             
-            <div class="projet col-md-4" id="projet6">
+            <div class="projet col-md-4 col-sm-6" id="projet6">
                        <div class="projet-present">
-                       <h3>Nom Projet</h3>
+                       <a href="#"><h3>Nom Projet</h3></a>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut vitae suscipit nobis hic, facere ipsam ad ratione explicabo inventore blanditiis eius provident unde, repudiandae necessitatibus nam voluptatem, iusto delectus! Consectetur!.</p>
                         </div>
             </div>
             
-            <div class="projet col-md-4" id="projet7">
+            <div class="projet col-md-4 col-sm-6" id="projet7">
                        <div class="projet-present">
-                       <h3>Nom Projet</h3>
+                       <a href="#"><h3>Nom Projet</h3></a>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut vitae suscipit nobis hic, facere ipsam ad ratione explicabo inventore blanditiis eius provident unde, repudiandae necessitatibus nam voluptatem, iusto delectus! Consectetur!.</p>
                         </div>
             </div>
             
-            <div class="projet col-md-4" id="projet8">
+            <div class="projet col-md-4 col-sm-6" id="projet8">
                        <div class="projet-present">
-                       <h3>Nom Projet</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut vitae suscipit nobis hic, facere ipsam ad ratione explicabo inventore blanditiis eius provident unde, repudiandae necessitatibus nam voluptatem, iusto delectus! Consectetur!.</p>
+                       <a href="#"><h3>Nom Projet</h3></a>
+                        </a><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut vitae suscipit nobis hic, facere ipsam ad ratione explicabo inventore blanditiis eius provident unde, repudiandae necessitatibus nam voluptatem, iusto delectus! Consectetur!.</p>
                         </div>
             </div>
             
-            <div class="projet col-md-4" id="projet9">
+            <div class="projet col-md-4 col-sm-6" id="projet9">
                        <div class="projet-present">
-                       <h3>Nom Projet</h3>
+                       <a href="#"><h3>Nom Projet</h3></a>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut vitae suscipit nobis hic, facere ipsam ad ratione explicabo inventore blanditiis eius provident unde, repudiandae necessitatibus nam voluptatem, iusto delectus! Consectetur!.</p>
                         </div>
             </div>

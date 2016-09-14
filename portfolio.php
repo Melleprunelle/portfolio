346fa6c14861b40a28e10955a862c32f7f3c733b
lise <lise.p.poirier@gmail.com>
2016-09-14 20:09:26 +0200

    hover accueil

--- portfolio.php
+++ portfolio.php
@@ -1,23 +1,46 @@
 <div class="portfolio col-md-offset-1 col-md-10">
     <div class="polaroid col-md-3">
-        <div class="projet col-md-12 col-sm-6" id="projet1">
-            <div class="projet-present"> <a href="projet1.php"><h3>Nom Projet</h3></a>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut vitae suscipit nobis hic, facere ipsam ad ratione explicabo inventore blanditiis eius provident unde, repudiandae necessitatibus nam voluptatem, iusto delectus! Consectetur!.</p>
+        <a href="projet1.php">
+            <div class="projet col-md-12 col-sm-6" id="projet1">
+                <div class="projet-present"> <i class="fa fa-search fa-3x" aria-hidden="true"></i> </div>
             </div>
-        </div>
+        </a>
     </div>
     <div class="polaroid col-md-3 col-md-offset-1">
         <div class="projet col-md-12 col-sm-6" id="projet1">
-            <div class="projet-present"> <a href="projet1.php"><h3>Nom Projet</h3></a>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut vitae suscipit nobis hic, facere ipsam ad ratione explicabo inventore blanditiis eius provident unde, repudiandae necessitatibus nam voluptatem, iusto delectus! Consectetur!.</p>
-            </div>
+            <a href="projet1.php">
+                <div class="projet col-md-12 col-sm-6" id="projet1">
+                    <div class="projet-present"> <i class="fa fa-search fa-3x" aria-hidden="true"></i> </div>
+                </div>
+            </a>
         </div>
     </div>
     <div class="polaroid col-md-3 col-md-offset-1">
-        <div class="projet col-md-12 col-sm-6" id="projet1">
-            <div class="projet-present"> <a href="projet1.php"><h3>Nom Projet</h3></a>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut vitae suscipit nobis hic, facere ipsam ad ratione explicabo inventore blanditiis eius provident unde, repudiandae necessitatibus nam voluptatem, iusto delectus! Consectetur!.</p>
+        <a href="projet1.php">
+            <div class="projet col-md-12 col-sm-6" id="projet1">
+                <div class="projet-present"> <i class="fa fa-search fa-3x" aria-hidden="true"></i> </div>
             </div>
-        </div>
+        </a>
+    </div>
+    <div class="polaroid col-md-3">
+        <a href="projet1.php">
+            <div class="projet col-md-12 col-sm-6" id="projet1">
+                <div class="projet-present"> <i class="fa fa-search fa-3x" aria-hidden="true"></i> </div>
+            </div>
+        </a>
+    </div>
+    <div class="polaroid col-md-3 col-md-offset-1">
+        <a href="projet1.php">
+            <div class="projet col-md-12 col-sm-6" id="projet1">
+                <div class="projet-present"> <i class="fa fa-search fa-3x" aria-hidden="true"></i> </div>
+            </div>
+        </a>
+    </div>
+    <div class="polaroid col-md-3 col-md-offset-1">
+        <a href="projet1.php">
+            <div class="projet col-md-12 col-sm-6" id="projet1">
+                <div class="projet-present"> <i class="fa fa-search fa-3x" aria-hidden="true"></i> </div>
+            </div>
+        </a>
     </div>
 </div>

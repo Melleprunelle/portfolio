32db2ab0854f22153a9be0b4b5e617daf5c41212
Lise Poirier <lise.p.poirier@gmail.com>
2016-09-22 14:10:06 +0200

    mise en route du cv

--- cv.php
+++ cv.php
@@ -10,11 +10,39 @@
                     <h1 class="col-md-12">curriculum vitae</h1>
                     <h2>Welcome to my C.V</h2>
                     <div class="separator-header2"></div>
-                    <section class="col-md-10 col-md-offset-1">
-
-                    </section>
-
                 </header>
+
+                <section class="cv col-md-10 col-md-offset-1">
+                    <div class="categorie col-md-3">
+                        <div class="present-cv col-md-12">
+                            <div>
+                                <img class="center-block img-responsive" src="img/lise.jpg" alt="">
+                            </div>
+                            <h3 class="text-center">Lise Poirier</h3>
+                            <p class="text-center">12 juin 1991 / 25 ans</p>
+                            <a class="col-md-12" href="lise-poirier.fr">www.lise-poirier.fr</a>
+                        </div>
+                        <div class="competences col-md-12">
+                            <div class="aptitude">
+                               <i class="fa fa-cog col-md-offset-1 col-md-1" aria-hidden="true"></i>
+                                <h4 class="col-md-9"> - Aptitudes</h4>
+                                <p class="col-md-offset-2">Créativité - 90%</p>
+                                <div class="creative col-md-offset-2 col-md-9"></div>
+                                <p class="col-md-offset-2">Détermination - 90%</p>
+                                <div class="creative col-md-offset-2 col-md-9"></div>
+                                <p class="col-md-offset-2">Gestion Projet - 80%</p>
+                                <div class="creative col-md-offset-2 col-md-7"></div>
+                                <p class="col-md-offset-2">Esprit d'équipe - 75%</p>
+                                <div class="creative col-md-offset-2 col-md-6"></div>                                <p class="col-md-offset-2">Curiosité - 85%</p>
+                                <div class="creative col-md-offset-2 col-md-8"></div>
+                                <p class="col-md-offset-2">Organisation - 80%</p>
+                                <div class="creative col-md-offset-2 col-md-7"></div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="categorie col-md-9"></div>
+                </section>
                 <!-- FOOTER -->
                 <?php include 'footer.php'; ?>
         </div>

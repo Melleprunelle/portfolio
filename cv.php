6abda52d465322a4dea865f89638cad472306136
Lise Poirier <lise.p.poirier@gmail.com>
2016-09-22 17:30:53 +0200

    mise en route du cv

--- cv.php
+++ cv.php
@@ -7,7 +7,7 @@
 
             <?php include 'nav.php'; ?>
                 <header id="startchange" class="container-fluid header cv-header">
-                    <h1 class="col-md-12">curriculum vitae</h1>
+                    <h1 class="col-md-12">CV - Web Développeur</h1>
                     <h2>Welcome to my C.V</h2>
                     <div class="separator-header2"></div>
                 </header>
@@ -20,7 +20,7 @@
                             </div>
                             <h3 class="text-center">Lise Poirier</h3>
                             <p class="text-center">12 juin 1991 / 25 ans</p>
-                            <a class="col-md-12" href="lise-poirier.fr">www.lise-poirier.fr</a>
+                            <a class="col-md-12">www.lise-poirier.fr</a>
                         </div>
                         <div class="competences col-md-12">
                             <div class="aptitude">
@@ -65,32 +65,67 @@
                                 <i class="fa fa-map-marker  col-md-offset-1 col-md-1" aria-hidden="true"></i>
                                 <h4 class="col-md-10"> - Contact</h4>
                                 <p class="col-md-offset-2">5 allée du Tage - 3500 Rennes</p>
-                                <a href="mailto:lise.p.poirier@gmail.com">
+                                
                                     <p class="col-md-offset-2">lise.p.poirier@gmail.com</p>
-                                </a>
+                        
                                 <p class="col-md-offset-2">06 52 84 43 53</p>
+                                
+                                <button type="button" class="btn btn-default col-md-offset-2 col-md-8">Imprimer C.V</button>
                             </div>
                         </div>
                     </div>
                     <div class="categorie col-md-9">
+                        <div class="presentation-cv col-md-offset-1 col-md-10">
+                            <i class="fa fa-heart fa-2x col-md-1" aria-hidden="true"></i>
+                            <h4>Welcome</h4>
+                            <div class="separator-cv col-md-12"></div>
+                            <h5 class="col-md-12">Graphiste Print & Développeur Web</h5>
+                            <p>Après avoir étudié dans la communication graphique et le développement informatique. Je suis aujourd'hui en capacité de participer activement dans ses domaines de la communication. Ma motivation et ma persévérance me sont des atouts au quotidien pour me perfectionner et satisfaire la demande des clients.</p>
+
+                        </div>
+
                         <div class="experience col-md-offset-1 col-md-10">
                             <i class="fa fa-area-chart fa-2x col-md-1" aria-hidden="true"></i>
                             <h4>Expériences</h4>
                             <div class="separator-cv col-md-12"></div>
                             <h5 class="col-md-12">Graphiste Print<small> - avril 2016 - Point Cardinal - Rennes</small></h5>
                             <p class="col-md-12"><strong>Réalisation des modifications de projets prints.</strong> - Création de communication pour l'entreprise Gourmet Catolard. - <strong>Réalisation de fiches recette, d'un flyer et d'un stop-rayon.</strong> - Modification du Kakémono.</p>
-                            
+
                             <h5 class="col-md-12">Graphiste Print<small> - septembre 2015 décembre 2015 - Hémisphère 4 - Rennes</small></h5>
-                            <p class="col-md-12"><strong>Création de logotypes pour des pharmacies du Grand Ouest.</strong> Pour chaque pharmacie, neuf propositions différentes étaient présentées aux clients. - <strong>Création de signalétique pour des pharmacies du Grand Ouest.</strong> Réalisation des maquettes de présentations à destinations du client. <strong>Mise en place de la communication externe des devantures et de la communication interne des pharmacies.</strong></p>     
-                                                   
+                            <p class="col-md-12"><strong>Création de logotypes pour des pharmacies du Grand Ouest.</strong> Pour chaque pharmacie, neuf propositions différentes étaient présentées aux clients. - <strong>Création de signalétique pour des pharmacies du Grand Ouest.</strong> Réalisation des maquettes de présentations à destinations du client. <strong>Mise en place de la communication externe des devantures et de la communication interne des pharmacies.</strong></p>
+
                             <h5 class="col-md-12">Graphiste Print<small> - juillet 2015 aout 2015 - Point Cardinal - Rennes</small></h5>
-                            <p class="col-md-12"> <strong>Création d'un visuel pour le stand de l'entreprise Vétagri</strong> spécialisée en agroalimentaire. Mise en place de la communication de ses divers pôles d'actions. - <strong>Création de packaging à destinations de la grande consommation pour la Marque "La Grande Hermine".</strong> <strong>Réalisation d'un visuel sobre et efficace d'un pack</strong> de "filets de cabillaud". - <strong>Lifting du logtype de la compagnie des pêches.</strong> - Création d'un livret A4 / 16 pages pour la société Koppert.</p>                            
-                            <h5 class="col-md-12">Graphiste Print<small> - avril 2016 - Point Cardinal - Rennes</small></h5>
-                            <p class="col-md-12">Réalisation des modifications de projets prints. - Création de communication pour l'entreprise Gourmet Catolard. - Réalisation de fiches recette, d'un flyer et d'un stop-rayon. - Modification du Kakémono.</p>                            
-                            <h5 class="col-md-12">Graphiste Print<small> - avril 2016 - Point Cardinal - Rennes</small></h5>
-                            <p class="col-md-12">Réalisation des modifications de projets prints. - Création de communication pour l'entreprise Gourmet Catolard. - Réalisation de fiches recette, d'un flyer et d'un stop-rayon. - Modification du Kakémono.</p>                            
-                            <h5 class="col-md-12">Graphiste Print<small> - avril 2016 - Point Cardinal - Rennes</small></h5>
-                            <p class="col-md-12">Réalisation des modifications de projets prints. - Création de communication pour l'entreprise Gourmet Catolard. - Réalisation de fiches recette, d'un flyer et d'un stop-rayon. - Modification du Kakémono.</p>
+                            <p class="col-md-12"> <strong>Création d'un visuel pour le stand de l'entreprise Vétagri</strong> spécialisée en agroalimentaire. Mise en place de la communication de ses divers pôles d'actions. - <strong>Création de packaging à destinations de la grande consommation pour la Marque "La Grande Hermine".</strong> <strong>Réalisation d'un visuel sobre et efficace d'un pack</strong> de "filets de cabillaud". - <strong>Lifting du logtype de la compagnie des pêches.</strong> - Création d'un livret A4 / 16 pages pour la société Koppert.</p>
+
+                            <h5 class="col-md-12">Graphiste Print<small> - février 2015 mars 2015 - Imagic - Rennes</small></h5>
+                            <p class="col-md-12"> <strong>Création d'un logotype</strong> pour "Les Ruchers du Pays de Rennes". - <strong>Création d'une affiche</strong> pour la coupe de France de natation en eau libre. - <strong>Création d'une chemise d'accueil pour la société ACE.</strong> - <strong>Création d'une affiche publicitaire basé sur le Tour de France.</strong> L'objectif était de retransmettre cette vision du Tour de France pour la communication de grande surface à destinations des consommateurs de produits laitiers.</p>
+                        </div>
+
+                        <div class="formation col-md-offset-1 col-md-10">
+                            <i class="fa fa-graduation-cap fa-2x col-md-1" aria-hidden="true"></i>
+                            <h4>Formations</h4>
+                            <div class="separator-cv col-md-12"></div>
+                            
+                             <h5 class="col-md-12">Formation Web Développeur spécialisée en Front<small> - 2017 - FACE SIMPLON - Rennes</small></h5>
+                            <p class="col-md-12">Cette formation apporte un lot de compétences dans le domaine du Web. Elle a pour but de formé dans le back ou dans le front.
+                            </p>
+                        
+                            <h5 class="col-md-12">Licence en Conception Graphique<small> - 2015 - Rennes 2 - Rennes</small></h5>
+                            <p class="col-md-12">Cette formation, de troisième année de licence, vise une insertion professionnelle en tant que graphiste, designer graphique, maquettiste ou infographiste dans le secteur de l’imprimé et du multimédia, en particulier appliqué au champ culturel. Elle a pour but l’acquisition d’une maîtrise des méthodologies du projet et des technologies numériques qui prennent en compte les évolutions actuelles.
+                            </p>
+
+                            <h5 class="col-md-12">Licence d'Arts Plastiques<small> - 2014 - Rennes 2 - Rennes</small></h5>
+                            <p class="col-md-12">Il s'agit d'une formation générale en arts plastiques, insistant sur une diversité d'approches, sur l'expérimentation créatrice, ainsi que sur la culture générale en art, ce qui garantit aux étudiants une importante capacité d'adaptation et les prépare à un environnement culturel, social et professionnel en évolution permanente.
+                            </p>
+
+                            <h5 class="col-md-12">BAC PRO AMA option communication graphique<small> - 2010 - Saint Pierre la Joliverie - Nantes</small></h5>
+                            <p class="col-md-12">Titulaire du Bac pro Artisanat et métier d’art option communication graphique, je maîtrise les techniques de mise en page et pourrais réaliser une maquette à partir d’un projet concepteur.
+                            </p>
+
+                            <h5 class="col-md-12">CAP Dessinateur d'exécution en communication<small> - 2008 - Saint Pierre la Joliverie - Nantes</small></h5>
+                            <p class="col-md-12">
+                                Titulaire du CAP Dessinateur d'exécution en communication je peux exécuter un projet, finaliser une maquette avant impression. Mon travail se situe entre le travail de création du concepteur et le travail d'édition et d'impression
+                            </p>
                         </div>
                     </div>
                 </section>
